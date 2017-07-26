f2426456d0d21c12bd7595e4238a94f2fceab64a
DarushDev <darushfaqiri@gmail.com>
2017-07-26 15:02:16 +0800

    Initialize views and menu in MainActivity

--- app/src/main/java/com/example/myandroidrecyclerviewgalacticon/MainActivity.java
+++ app/src/main/java/com/example/myandroidrecyclerviewgalacticon/MainActivity.java
@@ -1,13 +1,60 @@
 package com.example.myandroidrecyclerviewgalacticon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements ImageRequester.ImageRequesterResponse {
+
+    private ArrayList<Photo> mPhotosList;
+    private ImageRequester mImageRequester;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mPhotosList = new ArrayList<>();
+        mImageRequester = new ImageRequester(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void requestPhoto() {
+
+        try {
+            mImageRequester.getPhoto();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void receivedNewPhoto(final Photo newPhoto) {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mPhotosList.add(newPhoto);
+            }
+        });
+
     }
 }
